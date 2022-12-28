24fd1098c143e2eacc02c3214f03a4a469ea58ec
Hyojina <hjkim3009@gmail.com>
2022-12-28 12:17:19 +0900

    feat: 동작에 대한 기록 저장하기

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-// // 클래스형 -> 함수형으로 바꿀겁니다.
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Board from "./components/Board";
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <div className="game">
-        game
-        {/* game-board */}
-        <div className="game-board"></div>
-        <Board />
-        {/* game-info */}
-        <div className="game-info">
-          game-info
-          {/* status */}
-          <div></div>
-          {/* TODO */}
-          <ol></ol>
-        </div>
-      </div>
-    );
+const App = () => {
+  // history: 게임판의 모든 내역을 저장
+  const [history, setHistory] = useState([{ squares: Array(9).fill(null) }]);
+  // xIsNext: 플레이어(boolean타입으로 구분)
+  const [xIsNext, setXIsNext] = useState(true);
+  // current: 현재의 게임판 상황
+  const current = history[history.length - 1];
+  // winner: current를 이용하여 승자가 있는지 없는지 확인
+  const winner = calculateWinner(current.squares);
+
+  // status: winner를 이용하여 프린트되는 메시지를 변경
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = `Next Player: ${xIsNext ? "X" : "O"}`;
   }
-}
+
+  const handleClick = (i) => {
+    const newSquares = current.squares.slice();
+
+    if (calculateWinner(newSquares) || newSquares[i]) {
+      return; // 아래로 코드 진행 X
+    }
+    newSquares[i] = xIsNext ? "X" : "O";
+    setHistory([...history, { squares: newSquares }]);
+    setXIsNext((previousValue) => !previousValue);
+  };
+
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
+
+  return (
+    <div className="game">
+      {/* game-board */}
+      <div className="game-board"></div>
+      <Board squares={current.squares} onClick={(i) => handleClick(i)} />
+      {/* game-info */}
+      <div className="game-info">
+        {/* status */}
+        <div className="status">{status}</div>
+        <div></div>
+        {/* TODO */}
+        <ol></ol>
+      </div>
+    </div>
+  );
+};
+
+export default App;
--- src/components/Board.js
+++ src/components/Board.js
@@ -2,58 +2,13 @@ import React, { useState } from "react";
 import Square from "./Square";
 import "./Board.css";
 
-const Board = () => {
-  const [squares, setSquares] = useState(Array(9).fill(null));
-  const [xIsNext, setXIsNext] = useState(true);
-  const winner = calculateWinner(squares);
-  let status;
-  if (winner) {
-    status = "Winner: " + winner;
-  } else {
-    status = `Next Player: ${xIsNext ? "X" : "O"}`;
-  }
-
-  const handleClick = (i) => {
-    const newSquares = squares.slice();
-    if (calculateWinner(newSquares) || newSquares[i]) {
-      return; // 아래로 코드 진행 X
-    }
-    newSquares[i] = xIsNext ? "X" : "O";
-    setSquares(newSquares);
-    setXIsNext((previousValue) => !previousValue);
-  };
-
-  function calculateWinner(squares) {
-    const lines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
-    ];
-    for (let i = 0; i < lines.length; i++) {
-      const [a, b, c] = lines[i];
-      if (
-        squares[a] &&
-        squares[a] === squares[b] &&
-        squares[a] === squares[c]
-      ) {
-        return squares[a];
-      }
-    }
-    return null;
-  }
-
+const Board = ({ squares, onClick }) => {
   const renderSquare = (i) => {
-    return <Square value={squares[i]} onClick={() => handleClick(i)} />;
+    return <Square value={squares[i]} onClick={() => onClick(i)} />;
   };
 
   return (
     <div>
-      <div className="status">{status}</div>
       <div className="board-row">
         {renderSquare(0)}
         {renderSquare(1)}
